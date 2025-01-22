349c44c092c342df7b954c089e617846455589d8
Marc Watters <marc.watters@proton.me>
2025-01-22 08:19:44 -0500

    03 - Time Conversion (basic)
    Given a time in -hour AM/PM format, convert it to military (24-hour)
    time.

---
+++ 03-Time_Conversion/time_conversion.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+func timeConversion(s string) string {
+	parts := strings.Split(s, ":")
+	hour, _ := strconv.Atoi(parts[0])
+	minute := parts[1]
+	second := parts[2][:2]
+	suffix := parts[2][2:]
+
+	if suffix == "PM" && hour != 12 {
+		hour += 12
+	} else if suffix == "AM" && hour == 12 {
+		hour = 0
+	}
+
+	return fmt.Sprintf("%02d:%s:%s", hour, minute, second)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	s := readLine(reader)
+
+	result := timeConversion(s)
+
+	fmt.Fprintf(writer, "%s\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
